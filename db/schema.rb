87468eac0042af3bf6fb386467623455b71279d1
Marco Guzmán <ganondorf28@gmail.com>
2017-01-25 18:48:24 -0600

    Added new fields for Voter model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118033846) do
+ActiveRecord::Schema.define(version: 20170126004403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,12 @@ ActiveRecord::Schema.define(version: 20170118033846) do
     t.string   "section"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "latitude"
+    t.string   "longitude"
+    t.string   "phone_number"
+    t.string   "social_network"
+    t.string   "role"
+    t.string   "email"
   end
 
   create_table "zones", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
