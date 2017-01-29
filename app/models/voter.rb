2722cc555716b8818a0db5875b7f432ce9e46e1f
Marco Guzmán <ganondorf28@gmail.com>
2017-01-28 23:05:16 -0600

    Added validation when the voter is imported or created from app

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,3 +1,13 @@
 class Voter < ActiveRecord::Base
+
+  attr_accessor :imported
+
   validates_presence_of :full_name, :address, :electoral_number, :section
+  validates :latitude, :longitude, :phone_number, :social_network, :role, :email,
+    presence: true, if: :user_created_from_app?
+
+  def user_created_from_app?
+    return true if not imported
+  end
+
 end
