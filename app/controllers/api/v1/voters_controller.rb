b8271be98a94106ec1e1391aa34d0a1156ad1746
Marco Guzmán <ganondorf28@gmail.com>
2017-02-09 23:08:15 -0600

    Added json responses to create method for Voter

--- app/controllers/api/v1/voters_controller.rb
+++ app/controllers/api/v1/voters_controller.rb
@@ -2,37 +2,46 @@ class Api::V1::VotersController < ApplicationController
   respond_to :json
 
   def index
-    @voters = Voter.all
-    respond_with(@voters)
+    voters = Voter.all
+    respond_with(voters)
   end
 
   def new
-    @voter = Voter.new
+    voter = Voter.new
   end
 
   def create
-    @voter = Voter.new(voter_permit)
+    voter = Voter.new(voter_permit)
 
-    if @voter.save
-      puts 'display successful message'
+    if voter.save
+      render json: {
+        status: 200,
+        message: 'Successfully voter added',
+        voter: voter
+      }.to_json
     else
-      puts 'display error logs'
+      render json: {
+        status: 400,
+        message: 'Something went wrong'
+      }.to_json
     end
 
   end
 
   def show
-    @voter = Voter.find(params[:id])
+    voter = Voter.find(params[:id])
+    respond_with(voter)
   end
 
   def edit
-    @voter = Voter.find(params[:id])
+    voter = Voter.find(params[:id])
+    respond_with(voter)
   end
 
   def update
-    @voter = Voter.find(params[:id])
+    voter = Voter.find(params[:id])
 
-    if @voter.update(voter_permit)
+    if voter.update(voter_permit)
       puts 'display successful message'
     else
       puts 'display error logs'
@@ -40,9 +49,9 @@ class Api::V1::VotersController < ApplicationController
   end
 
   def destroy
-    @voter = Voter.find(params[:id])
+    voter = Voter.find(params[:id])
 
-    if @voter.destroy
+    if voter.destroy
       puts 'display successful message'
     else
       puts 'display error logs'
@@ -61,6 +70,8 @@ class Api::V1::VotersController < ApplicationController
       :phone_number,
       :social_network,
       :role,
+      :email,
+      :password,
       :imported,
       :user_id
     )
