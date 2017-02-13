4b5b78284fe40bd0ce0fb575ad54f740391f787e
Jaime Gonzalez <sacemax21@gmail.com>
2017-02-12 18:38:52 -0600

    added params to filter squares and sections

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -2,8 +2,13 @@ class Api::V1::SectionsController < Api::V1::ApiBaseController
   respond_to :json
 
   def index
-    sections = Section.all
-    respond_with(sections)
+    zone_id = params[:zone_id]
+    if zone_id
+      sections = Section.where(zone_id: zone_id)
+    else
+      sections = Section.all
+    end
+    respond_with sections
   end
 
 end
--- app/controllers/api/v1/squares_controller.rb
+++ app/controllers/api/v1/squares_controller.rb
@@ -2,8 +2,16 @@ class Api::V1::SquaresController < Api::V1::ApiBaseController
   respond_to :json
 
   def index
-    squares = Square.all
-    respond_with(squares)
+    zone_id = params[:zone_id]
+    section_id = params[:section_id]
+    if zone_id
+      squares = Square.where(zone_id: zone_id)
+    elsif section_id
+      squares = Square.where(section_id: section_id)
+    else
+      squares = Square.all
+    end
+    respond_with squares
   end
 
 end
