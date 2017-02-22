87300a814bfa34223222dfff77acdc3a758fd1da
Marco Guzmán <ganondorf28@gmail.com>
2017-02-21 22:42:37 -0600

    Added export variables for gmail user and password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "votanteapp@gmail.com",
-    :password             => "4321ZYXW",
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
