b425c8af2350c9e91d8af4e26f13160f543a775c
Jaime Gonzalez <sacemax21@gmail.com>
2017-02-25 14:34:40 -0600

    added doc for sections and squares

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -11,4 +11,14 @@ class Api::V1::SectionsController < Api::V1::ApiBaseController
     respond_with sections
   end
 
+  swagger_controller :zones, "Sections Management", resource_path: "/api/v1"
+  swagger_api :index do
+    summary "Fetches all sections"
+    notes "This lists all the sections"
+    param :query, :zone_id, :string, :optional, "Filter per zone_id"
+    response :unauthorized
+    response :not_acceptable, "The request you made is not acceptable"
+    response :requested_range_not_satisfiable
+  end
+
 end
--- app/controllers/api/v1/zones_controller.rb
+++ app/controllers/api/v1/zones_controller.rb
@@ -10,8 +10,6 @@ class Api::V1::ZonesController < Api::V1::ApiBaseController
   swagger_api :index do
     summary "Fetches all zones"
     notes "This lists all the zones"
-    param :query, :page, :integer, :optional, "Page number"
-    param :path, :nested_id, :integer, :optional, "Team Id"
     response :unauthorized
     response :not_acceptable, "The request you made is not acceptable"
     response :requested_range_not_satisfiable
