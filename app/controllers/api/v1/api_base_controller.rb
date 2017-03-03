8c229cd35ad82b3fc61c34aaac2c2014471ef54c
Jaime Gonzalez <sacemax21@gmail.com>
2017-03-03 00:35:53 -0600

    making work the auth_user

--- app/controllers/api/v1/api_base_controller.rb
+++ app/controllers/api/v1/api_base_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ApiBaseController < ApplicationController
-  include DeviseTokenAuth::Concerns::SetUserByToken
+  include Devise::Controllers::Helpers
+  before_action :authenticate_voter!
   protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
   respond_to :json
 end
