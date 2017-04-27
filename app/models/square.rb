42c2841a9fa3ea8d792ea26196d34ff63903b61b
Marco Guzmán <ganondorf28@gmail.com>
2017-04-26 22:55:54 -0500

    Added visibility of the assigned coordinators for zone, section, or square in the admin panel

--- app/models/square.rb
+++ app/models/square.rb
@@ -1,7 +1,7 @@
 class Square < ActiveRecord::Base
   belongs_to :section
   belongs_to :zone
-  belongs_to :voter
+  belongs_to :voter, foreign_key: :coordinator_id
 
   validates_presence_of :name, :section_id, :zone_id
   validates_length_of :name, maximum: 100
